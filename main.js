7b3d57b7ffe3d8c74fa90b09d5449c417bd331bb
hbarufaldi-ux <hbarufaldi@gmail.com>
2026-05-10 18:00:31 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const map = L.map('map').setView([-32.41, -63.24], 14);
 
 // SATÉLITE DE GOOGLE (Esta URL es la más compatible de todas)
-L.tileLayer('https://google.cn{x}&y={y}&z={z}', {
-    attribution: 'Google Satellite'
+L.tileLayer('https://arcgisonline.com{z}/{y}/{x}', {
+    attribution: 'Tiles &copy; Esri'
 }).addTo(map);
+
 
 
 const grupoLotes = new L.FeatureGroup().addTo(map);
