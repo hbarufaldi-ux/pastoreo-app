8e64632f284b42de4724caed990f5fc0d46acc6b
hbarufaldi-ux <hbarufaldi@gmail.com>
2026-05-10 14:35:44 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const map = L.map('map').setView([-32.41, -63.24], 13);
 
-// Satélite de Google (Más estable para ver el campo real)
-L.tileLayer('https://google.com{x}&y={y}&z={z}', {
-    attribution: 'Google Maps'
+// Satélite Esri - URL robusta para evitar el fondo oscuro
+L.tileLayer('https://arcgisonline.com{z}/{y}/{x}', {
+    attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, GeoEye'
 }).addTo(map);
 
 const grupoLotes = new L.FeatureGroup().addTo(map);
@@ -12,24 +12,27 @@ function cargarLote(archivo, color, nombreTipo) {
         .then(res => res.json())
         .then(data => {
             L.geoJSON(data, {
+                // ESTO QUITA LOS MARCADORES AZULES
+                pointToLayer: (feature, latlng) => { return null; },
                 style: { 
                     color: "white", 
                     weight: 1, 
                     fillColor: color, 
-                    fillOpacity: 0.4 // Bajamos opacidad para ver el pasto abajo
+                    fillOpacity: 0.4 
                 },
                 onEachFeature: (feature, layer) => {
                     const nombre = (feature.properties && feature.properties.name) ? feature.properties.name : nombreTipo;
-                    layer.bindPopup(`<b>Tipo: ${nombreTipo}</b><br>Lote: ${nombre}`);
+                    layer.bindPopup(`<b>${nombreTipo}</b><br>Lote: ${nombre}`);
                 }
             }).addTo(grupoLotes);
             
-            map.fitBounds(grupoLotes.getBounds());
+            if (grupoLotes.getLayers().length > 0) {
+                map.fitBounds(grupoLotes.getBounds());
+            }
         })
         .catch(err => console.error("Error cargando:", archivo));
 }
 
-// Carga de archivos
 cargarLote('lotes_tambo.geojson', '#3498db', 'Tambo');
 cargarLote('lotes_cria.geojson', '#e67e22', 'Cría');
 cargarLote('lote_agricultura.geojson', '#f1c40f', 'Agricultura');
