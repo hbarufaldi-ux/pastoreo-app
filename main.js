dde546051060e1ea20f819201c4f611be20e1e10
hbarufaldi-ux <hbarufaldi@gmail.com>
2026-05-10 17:28:16 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const map = L.map('map').setView([-32.41, -63.24], 13);
 
-// CAPA SATELITAL (Asegúrate de que esta URL sea la correcta para que no se vea blanco)
-L.tileLayer('https://arcgisonline.com{z}/{y}/{x}', {
-    attribution: 'Tiles &copy; Esri'
+// Satélite de Google (Más estable para ver el campo real)
+L.tileLayer('https://google.com{x}&y={y}&z={z}', {
+    attribution: 'Google Maps'
 }).addTo(map);
 
 const grupoLotes = new L.FeatureGroup().addTo(map);
@@ -12,24 +12,24 @@ function cargarLote(archivo, color, nombreTipo) {
         .then(res => res.json())
         .then(data => {
             L.geoJSON(data, {
-                // Esto quita los marcadores azules y deja solo el polígono
-                pointToLayer: (feature, latlng) => { return null; }, 
                 style: { 
                     color: "white", 
-                    weight: 2, 
+                    weight: 1, 
                     fillColor: color, 
-                    fillOpacity: 0.6 
+                    fillOpacity: 0.4 // Bajamos opacidad para ver el pasto abajo
                 },
                 onEachFeature: (feature, layer) => {
                     const nombre = (feature.properties && feature.properties.name) ? feature.properties.name : nombreTipo;
-                    layer.bindPopup(`<b>${nombre}</b>`);
+                    layer.bindPopup(`<b>Tipo: ${nombreTipo}</b><br>Lote: ${nombre}`);
                 }
             }).addTo(grupoLotes);
+            
             map.fitBounds(grupoLotes.getBounds());
         })
-        .catch(err => console.error("Error:", err));
+        .catch(err => console.error("Error cargando:", archivo));
 }
 
+// Carga de archivos
 cargarLote('lotes_tambo.geojson', '#3498db', 'Tambo');
 cargarLote('lotes_cria.geojson', '#e67e22', 'Cría');
 cargarLote('lote_agricultura.geojson', '#f1c40f', 'Agricultura');
